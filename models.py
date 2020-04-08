d8352981d0632747a5ff99fb26351199b3ca0c31
LostLevi <nootoo@ya.ru>
2020-04-09 01:43:36 +0300

    add db

---
+++ models.py
@@ -0,0 +1,11 @@
+from app import db
+from datetime import datetime
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(140))
+    slug = db.Column(db.String(140, unique = True))
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default = datetime.now())
+
+    def __init__(self, *args, **kwargs):
